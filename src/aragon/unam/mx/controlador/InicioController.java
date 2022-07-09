483a196086e0343917b0a534fb272cb372bc8ff7
ZekiBestia <ezequielor73@gmail.com>
2022-07-09 17:01:35 -0500

    Se agrega el rdb y nuevas caracteristicas

--- src/aragon/unam/mx/controlador/InicioController.java
+++ src/aragon/unam/mx/controlador/InicioController.java
@@ -1,10 +1,12 @@
 package aragon.unam.mx.controlador;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import aragon.unam.mx.modelo.Persona;
+import aragon.unam.mx.modelo.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.css.PseudoClass;
@@ -16,11 +18,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.image.Image;
@@ -31,9 +35,31 @@ import javafx.util.Callback;
 import javafx.util.converter.FloatStringConverter;
 import javafx.scene.control.TextField;
 
-public class InicioController implements Initializable {
+public class InicioController extends BaseController implements Initializable {
+	private static final String RadioButton = null;
+	private String mensajes = "";
+
 	private ObservableList<Persona> datos = FXCollections.observableArrayList();
 
+	@FXML
+	private ToggleGroup estadoCivil;
+
+	@FXML
+	private RadioButton rdbCasado;
+
+	@FXML
+	private RadioButton rdbFemenino;
+
+	@FXML
+	private RadioButton rdbSoltero;
+
+	@FXML
+	private RadioButton rdbUnionLibre;
+
+	@FXML
+	private RadioButton rdbMasculino;
+	@FXML
+	private TextField txtCurp;
 	@FXML
 	private ComboBox<String> cbbEjemplo;
 	@FXML
@@ -105,13 +131,13 @@ public class InicioController implements Initializable {
 		captura.getCredito().set(chcCredito.getValue());
 		captura.getApellidoPaterno().set(txtApellidoPaterno.getText());
 		captura.getApellidoMaterno().set(txtApellidoMaterno.getText());
-		//captura.getFechaNacimiento().set(dtpFechaNacimiento.getAccessibleText());
-		//captura.getNacionalidad().set(chcNacionalidad.getValue());
-		//captura.getSexo().set(chcSexo.getValue());
-		//captura.getGradoEstudios().set(chcEstudio.getValue());
-		//captura.getTelefonoCasa().set(txtCasa.getText());
-		captura.getTelefonoCelular().set(txtCelular.getText());
+		// captura.getFechaNacimiento().set(dtpFechaNacimiento.getAccessibleText());
 		captura.getRfc().set(txtRfc.getText());
+		captura.getNacionalidad().set(chcNacionalidad.getValue());
+		// captura.getSexo().set(chcSexo.getValue());
+		// captura.getGradoEstudios().set(chcEstudio.getValue());
+		// captura.getTelefonoCasa().set(txtCasa.getText());
+		captura.getTelefonoCelular().set(txtCelular.getText());
 		datos.add(captura);
 		this.limpiar();
 
@@ -124,26 +150,55 @@ public class InicioController implements Initializable {
 		this.txtApellidoMaterno.clear();
 		this.txtApellidoPaterno.clear();
 		this.txtCuenta.clear();
-		//this.txtCasa.clear();
+		this.txtCurp.clear();
+		this.txtRfc.clear();
+		// this.txtCasa.clear();
 		this.txtCelular.clear();
 		this.txtMonto.clear();
-		//this.chcCredito.getItems().clear();
-		//this.chcEstudio.getItems().clear();
-		//this.chcNacionalidad.getItems().clear();
-		//this.dtpFechaNacimiento.getEditor().clear();
-		this.txtRfc.clear();
-		//this.chcSexo.getItems().clear();
+		this.chcCredito.getItems().clear();
+		// this.chcEstudio.getItems().clear();
+		this.chcNacionalidad.getItems().clear();
+		this.dtpFechaNacimiento.getEditor().clear();
+		// this.chcSexo.getItems().clear();
+		this.rdbCasado.setSelected(false);
+		this.rdbCasado.requestFocus();
+		this.rdbCasado.getTypeSelector();
 
 	}
 
 	@FXML
 	void salir(ActionEvent event) {
-
+		this.cerrarVentana(btnSalir);
+	}
+	
+	void accionVerificar(ActionEvent event) {
+		if (this.verificar() && verificacion) {
+			System.out.println("Valido");
+		} else {
+			this.mensajes += "Ningun campo debe estar en rojo";
+			this.ventanaEmergente("Datos no validos", "Por favor valida la información,", mensajes);
+			this.mensajes = "";
+		}
 	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		this.chcCredito.getItems().addAll("12","6","28");
+		this.verificarEntrada(txtRfc, TipoError.RFC);
+		this.verificarEntrada(txtNombre, TipoError.IDENTIFICACION);
+		this.verificarEntrada(txtApellidoPaterno, TipoError.IDENTIFICACION);
+		this.verificarEntrada(txtApellidoMaterno, TipoError.IDENTIFICACION);
+		this.verificarEntrada(txtCelular, TipoError.CELULAR);
+		this.verificarEntrada(txtCurp, TipoError.CURP);
+		this.verificarEntrada(txtCuenta, TipoError.CUENTA);
+		this.chcNacionalidad.getItems().addAll("Canadiense", "Estadounidense", "Alemana", "Rusa");
+		this.chcCredito.getItems().addAll("12", "6", "28");
+		this.rdbFemenino.setUserData(new RadioButton("Ana"));
+		this.rdbMasculino.setUserData(new RadioButton("Pedro"));
+		this.rdbCasado.setUserData(new RadioButton("casado"));
+		this.rdbSoltero.setUserData(new RadioButton("soltero"));
+		this.rdbUnionLibre.setUserData(new RadioButton("libre"));
+		// this.dtpFechaNacimiento.getEditor();
+		// this.dtpFechaNacimiento.getValue(new DatePicker(LocalDate.now());
 		this.tbcNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
 		this.tbcNombre.setCellFactory(TextFieldTableCell.forTableColumn());
 		this.tbcNombre.setOnEditCommit(t -> {
@@ -157,15 +212,14 @@ public class InicioController implements Initializable {
 		this.tbcCuenta.setOnEditCancel(t -> {
 			((Persona) t.getTableView().getItems().get(t.getTablePosition().getRow())).cuentaProperty()
 					.set(t.getNewValue());
-			//this.tbwTabla.refresh();
+			// this.tbwTabla.refresh();
 		});
-		
 
 		this.tbcMonto.setCellValueFactory(new PropertyValueFactory<>("monto"));
 		this.tbcMonto.setCellFactory(TextFieldTableCell.forTableColumn(new FloatStringConverter()));
-		this.tbcMonto.setOnEditCommit(t -> {			
+		this.tbcMonto.setOnEditCommit(t -> {
 			((Persona) t.getTableView().getItems().get(t.getTablePosition().getRow())).montoProperty()
-					.set(t.getNewValue());			
+					.set(t.getNewValue());
 			this.tbwTabla.refresh();
 		});
 
@@ -193,20 +247,20 @@ public class InicioController implements Initializable {
 						setGraphic(null);
 						setText(null);
 					} else {
-						Image img=new Image(getClass().getResourceAsStream("/aragon/unam/mx/recursos/boton.jpg"));
+						Image img = new Image(getClass().getResourceAsStream("/aragon/unam/mx/recursos/boton.jpg"));
 						Button button = new Button();
-						button.setGraphic(new ImageView(img));						
-						button.setOnMouseClicked((MouseEvent evento) -> {							
-							int fila=tbwTabla.getSelectionModel().getSelectedIndex();
-							if(fila!=-1) {
+						button.setGraphic(new ImageView(img));
+						button.setOnMouseClicked((MouseEvent evento) -> {
+							int fila = tbwTabla.getSelectionModel().getSelectedIndex();
+							if (fila != -1) {
 								datos.remove(fila);
 							}
-							
+
 						});
-						
+
 						HBox hbox = new HBox(button);
 						hbox.setStyle("-fx-alignment:center");
-						HBox.setMargin(button, new Insets(2, 2, 0, 3));						
+						HBox.setMargin(button, new Insets(2, 2, 0, 3));
 						setGraphic(hbox);
 						setText(null);
 					}
@@ -218,11 +272,85 @@ public class InicioController implements Initializable {
 
 		this.tbcAccion.setCellFactory(celda);
 		this.tbwTabla.setItems(datos);
-		
-		
-		
-		
-		
+
 	}
+	private boolean verificar() {
+		boolean valido = true;
+		if (this.txtCuenta.getText() == null || this.txtCuenta.getText().trim().isEmpty()) {
+			valido = false;
+			this.mensajes += "Número de Cuenta no valido\n";
+		}
+		
+		if ((this.txtCuenta.getText() == null)
+				|| (this.txtCuenta.getText() != null && !this.txtCuenta.getText().isEmpty())) {
+			if (!this.cuentaValido) {
+				this.mensajes += "La cuenta no es valida, tienen que ser 9 digitos\n";
+				valido = false;
+			}
+		}
+		
+		
+		if (this.chcCredito.getSelectionModel().getSelectedIndex() == -1) {
+			valido = false;
+			this.mensajes += "Seleccione un plazo\n";
+		}
+		
+		if (!this.rdbFemenino.isSelected() && !this.rdbMasculino.isSelected()) {
+			valido = false;
+			this.mensajes += "Seleccione un sexo\n";
+		}
+		
+		
+		if (!this.rdbCasado.isSelected() && !this.rdbSoltero.isSelected() && !this.rdbUnionLibre.isSelected()) {
+			valido = false;
+			this.mensajes += "Seleccione un sexo\n";
+		}
+		if ((this.txtNombre.getText() == null)
+				|| (this.txtNombre.getText() != null && this.txtNombre.getText().isEmpty())) {
+			this.mensajes += "El nombre no es valido.Complete el espacio\n";
+			valido = false;
+		}
+
+		if ((this.txtApellidoPaterno.getText() == null) || (this.txtApellidoPaterno.getText().isEmpty())) {
+			this.mensajes += "El apellido paterno no es valido , complete el espacio\n";
+			valido = false;
+		}
+
+		if ((this.txtApellidoPaterno.getText() == null)
+				|| (this.txtApellidoPaterno.getText() != null && !this.txtApellidoPaterno.getText().isEmpty())) {
+			if (!this.identificacionValida) {
+				this.mensajes += "El Apellido paterno no es valido.Solo escribe un apellido y no se permiten numeros\n";
+				valido = false;
+			}
+		}
+
+		if ((this.txtApellidoMaterno.getText() == null)
+				|| (this.txtApellidoMaterno.getText() != null && this.txtApellidoMaterno.getText().isEmpty())) {
+			this.mensajes += "El apellido materno no es valido , complete el espacio\n";
+			valido = false;
+		}
+		if ((this.txtApellidoMaterno.getText() == null)
+				|| (this.txtApellidoMaterno.getText() != null && !this.txtApellidoMaterno.getText().isEmpty())) {
+			if (!this.identificacionValida) {
+				this.mensajes += "El apellido, esta mal estructurado.Solo escribe un apellido y no se permiten numeros\n";
+				valido = false;
+			}
+		}
+		
+		if ((this.txtRfc.getText() == null) || (this.txtRfc.getText() != null && this.txtRfc.getText().isEmpty())) {
+			this.mensajes += "El RFC no es valido , complete el espacio\n";
+			valido = false;
+		}
+		if ((this.txtRfc.getText() == null) || (this.txtRfc.getText() != null && !this.txtRfc.getText().isEmpty())) {
+			if (!this.rfcValido) {
+				this.mensajes += "El RFC no es valido , minimo=13 , maximo=13 caracteres\n";
+				valido = false;
+			}
+
+		}
+		
+		return valido;
+	}
+	
 
 }
---
+++ src/aragon/unam/mx/modelo/TipoError.java
@@ -0,0 +1,5 @@
+package aragon.unam.mx.modelo;
+
+public enum TipoError {
+	CUENTA,IDENTIFICACION,RFC,CURP,CELULAR,MONTO
+}
